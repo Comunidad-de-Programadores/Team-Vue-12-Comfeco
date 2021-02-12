47de01465b1127c11eacddfcea6aeb97b7703edb
Jhoel Flores Alejo <joe0zf@gmail.com>
2021-02-12 12:11:36 -0500

    Vista para politica de privacidad comfeco creada

---
+++ src/views/PoliciesComfeco.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="polices-comfeco-container">
+        <h1>Políticas de privacidad</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+@import url('../assets/css/colors.css');
+.polices-comfeco-container{
+    width: 100%;
+    height: 100vh;
+    background-color:var(--bgmain);
+    color:white;
+}
+</style>
